2fe71e5f62af66a0a7eed7d63ce7edd5c91b2687
HuralOmer <huralomer@gmail.com>
2025-09-11 15:31:17 +0300

    update

--- extensions/theme-extension/assets/tracker.js
+++ extensions/theme-extension/assets/tracker.js
@@ -6,9 +6,13 @@
   const TRACKING_URL = 'https://hrl-tracking-app-production.up.railway.app';
   const SHOP_DOMAIN = window.location.hostname;
   
-  // Generate session ID
+  // Generate session ID (UUID format)
   function generateSessionId() {
-    return 'session_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
   
   // Get or create session ID
